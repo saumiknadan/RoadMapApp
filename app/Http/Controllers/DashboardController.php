a134a3e52d757ca35cafad7aae8ed3608bc29631
saumik chowdhury <chowdhurysaumik@gmail.com>
2025-06-18 11:24:55 +0600

    breeze install

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        try {
+            return view('dashboard');
+            // return view('admin.dashboard');
+
+        } catch (\Throwable $th) {
+            session()->flash('error', 'Something went wrong');
+            return redirect()->back();
+        }
+    }
+}
